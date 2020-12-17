51ad1420fc62d578c3ec0e68e5ccd40e76b059de
nasrinsamad19 <nasrinsamad19@gmail.com>
2020-12-17 15:09:05 +0530

    Added Homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qmis/screens/homepage/view/home_ui.dart';
 import 'package:qmis/screens/loginpage/view/ui.dart';
 
 void main() => runApp(MyApp());
@@ -7,6 +8,6 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext buildContext) {
     return MaterialApp(
-      home: Login(),
+      home: HomeView(),
     );
   }}
---
+++ lib/screens/homepage/view/home_ui.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => new _State();
+
+}
+class _State extends State<HomeView>{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          actions: [
+            IconButton(icon: Padding(padding: EdgeInsets.all(7.0),
+            child: Image.asset('assets/images/home_ui/calendar.png'),), onPressed: null),
+            Container(padding: EdgeInsets.fromLTRB(15, 5, 15, 5),),
+            IconButton(icon: Padding(padding: EdgeInsets.all(7.0),
+              child: Image.asset('assets/images/home_ui/notification.png'),), onPressed: null),
+            Container(padding: EdgeInsets.fromLTRB(15, 5, 15, 5),),
+            IconButton(icon: Padding(padding: EdgeInsets.all(7.0),
+              child: Image.asset('assets/images/home_ui/profile.png'),), onPressed: null),
+            Container(padding: EdgeInsets.fromLTRB(15, 5, 15, 5),),
+            IconButton(icon: Padding(padding: EdgeInsets.all(7.0),
+              child: Image.asset('assets/images/home_ui/side_menu_icon.png'),), onPressed: null),
+            Container(padding: EdgeInsets.fromLTRB(10, 5, 10, 5),),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.black,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: IconButton(icon: Padding(padding: EdgeInsets.all(7.0),
+                child: Image.asset('assets/images/home_ui/restaurant.png'),), onPressed: null),
+              title: Text("Dining"),
+            ),
+            BottomNavigationBarItem(
+              icon: IconButton(icon: Padding(padding: EdgeInsets.all(7.0),
+                child: Image.asset('assets/images/home_ui/gift_shop.png'),), onPressed: null),
+              title: Text("Gift Shop"),
+            ),
+            BottomNavigationBarItem(
+              icon: IconButton(icon: Padding(padding: EdgeInsets.all(7.0),
+                child: Image.asset('assets/images/home_ui/culture_pass.png'),), onPressed: null),
+              title: Text("Culture Pass"),
+            ),
+            BottomNavigationBarItem(
+              icon: IconButton(icon: Padding(padding: EdgeInsets.all(7.0),
+                child: Image.asset('assets/images/home_ui/more.png'),), onPressed: null),
+              title: Text("More"),
+            ),
+          ],
+          onTap:null ,),
+      ),
+    );
+  }
+}
